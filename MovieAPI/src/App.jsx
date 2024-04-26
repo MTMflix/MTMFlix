23f1ed1cd43f835c3f338744e0f028887605d7ce
Meagan Hollingsworth <mhollingsworth@google.com>
2024-04-26 17:07:45 -0400

    created the options tab

--- MovieAPI/src/App.jsx
+++ MovieAPI/src/App.jsx
@@ -3,12 +3,15 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import Navbar from "./components/layout/Navbar"
 // import Dashboard from "./components/layout/Dashboard";
+import OptionsTabs from "./components/layout/OptionsTabs";
 
 class App extends Component{
   render(){
     return (
       <div className="App">
         <Navbar/>
+        <OptionsTabs/>
+
       </div>
     );
   }
