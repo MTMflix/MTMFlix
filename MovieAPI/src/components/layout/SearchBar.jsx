8344a93fb82dbdd9148400a0e5a04a258e9778f4
Tina Sun <suntina@google.com>
2024-05-03 16:07:51 -0400

    add general search results

--- MovieAPI/src/components/layout/SearchBar.jsx
+++ MovieAPI/src/components/layout/SearchBar.jsx
@@ -1,14 +1,51 @@
-import React, { Component } from 'react'
-import "./SearchBar.css"
-import {FaSearch} from "react-icons/fa"
+import React, { Component, useState } from 'react';
+import "./SearchBar.css";
+import { FaSearch } from "react-icons/fa";
 
-export default class SearchBar extends Component {
-  render() {
-    return (
-      <div className='input-wrapper'>
-        <FaSearch id='search-icon'/>
-        <input placeholder=' search to browse...' />
-      </div>
-    )
-  }
+const url = 'https://ott-details.p.rapidapi.com/advancedsearch?start_year=1970&end_year=2020&min_imdb=6&max_imdb=7.8&genre=action&language=english&type=movie&sort=latest';
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'ec9ef9a28dmsh275e37278b75032p13c95bjsn38291f0cc22b',
+        'X-RapidAPI-Host': 'ott-details.p.rapidapi.com'
+    }
+};
+
+function SearchBar() {
+  const [input, setInput] = useState("");
+
+  const fetchData = async (value) => {
+    try {
+      const response = await fetch(url, options);
+      const data = await response.json();
+      console.log(data); 
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  const handleChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const enterKey = (event) => {
+    if (event.key === 'Enter') {
+      fetchData(input); // Pass the current value of input state
+    }
+  };
+  
+
+  return (
+    <div className='input-wrapper'>
+      <FaSearch id='search-icon' />
+      <input
+        placeholder='search next watch '
+        value={input}
+        onChange={handleChange}
+        onKeyDown={enterKey}
+      />
+    </div>
+  );
 }
+
+export default SearchBar;
