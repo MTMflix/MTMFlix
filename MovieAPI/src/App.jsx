74da3530818bb9c57c4fd96b7e82b896d8fad7d5
Tina Sun <suntina@google.com>
2024-05-09 16:54:47 -0400

    partial app components layout fix

--- MovieAPI/src/App.jsx
+++ MovieAPI/src/App.jsx
@@ -2,27 +2,31 @@ import React, { Component } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import Navbar from "./components/layout/Navbar"
-// import Dashboard from "./components/layout/Dashboard";
 import OptionsTabs from "./components/layout/OptionsTabs";
-import SearchResults from "./components/layout/SearchResults";
 // import SearchResults from "./components/layout/SearchResults";
+// import SearchResults from "./components/layout/SearchResults";
+import Dashboard from "./components/layout/Dashboard";
 
 class App extends Component{
   render(){
     return (
-      <Router>
+      // <Router>
         <div className="App">
-          <Navbar/>
-          <OptionsTabs/>
-
+            <Navbar/>
+          <div className="options">
+            <OptionsTabs/>
+          </div>
+          <div className = "dashboard">
+            <Dashboard/>
+          </div>
           {/* <SearchResults results ={results}/> */}
         </div>
-        <Routes>
+        /* <Routes>
           <Route
             path="/searchResults"
             element={<SearchResults/>}/>
         </Routes>
-      </Router>
+      </Router> */
     );
   }
 }
