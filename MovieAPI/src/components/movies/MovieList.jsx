6b0f0b46c65d0ba0f1ebbb88f43c891f543c031a
Michael Vela <mikevela@google.com>
2024-05-16 15:54:54 -0500

    grid layout of movies on dashboard site, added fetchurl tp movielist file, replaced component with function appjsx file

--- MovieAPI/src/components/movies/MovieList.jsx
+++ MovieAPI/src/components/movies/MovieList.jsx
@@ -1,19 +1,38 @@
-import React, {useEffect, useState} from "react";
-import SearchBar from "../layout/SearchBar";
+import React, { useState, useEffect } from "react";
 
-const url = 'https://ott-details.p.rapidapi.com/getnew?region=US&page=1';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'ec9ef9a28dmsh275e37278b75032p13c95bjsn38291f0cc22b',
-		'X-RapidAPI-Host': 'ott-details.p.rapidapi.com'
-	}
-};
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
+function MovieList () {
+
+    
+    const {VITE_MOVIE_API_KEY} = import.meta.env;
+
+    const [movieList, setMovieList] = useState([]);
+    const [select, setSelect] = useState("");
+
+    const getMovieList = async() => {
+        try {
+          fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${VITE_MOVIE_API_KEY}`)
+          .then(res => res.json())
+          .then(json => setMovieList(json.results))  
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
+    useEffect(() => {
+        getMovieList()
+    }, [])
+
+    return (
+        <div>
+            <button onClick={() => setSelect("movie")}>Movie</button>
+            {movieList.map((data) => {
+                return <>
+                <img style={{width:"300px", height:"250px", marginTop:"10px", marginLeft:"10px"}} src={`https://image.tmdb.org/t/p/w500${data?.poster_path}`} />
+                </>
+            })}
+        </div>
+    )
 }
+
+export default MovieList;
