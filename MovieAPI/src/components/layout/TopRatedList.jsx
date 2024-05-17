49e7e48fc4d27319f75a70fdb15c3783007257b8
Meagan Hollingsworth <mhollingsworth@google.com>
2024-05-17 15:27:50 -0400

    added the Link from router-dom to make the top rated movies linkable

--- MovieAPI/src/components/layout/TopRatedList.jsx
+++ MovieAPI/src/components/layout/TopRatedList.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
 import "./OptionsTabs.css";
 
 function TopRatedList () {
@@ -36,7 +37,7 @@ function TopRatedList () {
                 <ul>
                     {topRated.map((movie) => (
                         <li key={movie.id}>
-                            {movie.title}
+                            <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
                         </li>
                     ))}
                 </ul>
