08ce1fec3f07abde27e633b590e8b936343dfae2
Meagan Hollingsworth <mhollingsworth@google.com>
2024-06-04 11:42:40 -0400

    Made the logo a clickable link back to the home page

--- MovieAPI/src/components/layout/Navbar.jsx
+++ MovieAPI/src/components/layout/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import MTMflix_logo from './logo/MTMflix_logo.png'
 import SearchBar from './SearchBar';
 import Nav from 'react-bootstrap/Nav';
@@ -8,10 +8,16 @@ import Nav from 'react-bootstrap/Nav';
 
 function Navbar(){
     const [results, setResults] = useState([]);
+    const navigate = useNavigate();
+
+    const handleLogoClick = () => {
+        navigate('/');
+    }
         return(
             <div>
                 <nav className='navbar navbar-expand-md navbar-dark bg-dark fixed-top'>
-                    <a className="navbar-brand col-sm-3 col-md-2 mr-0 align-items-center"> 
+                    <a className="navbar-brand col-sm-3 col-md-2 mr-0 align-items-center cursor pointer"
+                        onClick={handleLogoClick}> 
                     <img src={MTMflix_logo} alt="MTMflix logo"/>
                     </a>
                     <h1 className="navbar-brand"><SearchBar setResults={setResults}/></h1>
