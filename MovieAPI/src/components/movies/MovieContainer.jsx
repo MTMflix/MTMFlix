662dfa307737886a85ed51a41bca724393b6ec8b
Tina Sun <suntina@google.com>
2024-08-16 01:52:05 -0400

    add favorites and quick view feature

--- MovieAPI/src/components/movies/MovieContainer.jsx
+++ MovieAPI/src/components/movies/MovieContainer.jsx
@@ -4,26 +4,35 @@ import MovieDetails from "./MovieDetails";
 
 function MovieContainer(props) {
 
-    const {data, changeMovieData} = props;
-    const [isModalOpen, setIsModalOpen] = useState(false);
+  const { data, changeMovieData } = props;
+  const [isLiked, setIsLiked] = useState(false); // State for like button
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const handleClick = () => {
-        changeMovieData(data);
-        setIsModalOpen(true);
-    };
+  const handleClick = () => {
+    changeMovieData(data);
+    setIsModalOpen(true);
+  };
 
-    const closeModal = () => setIsModalOpen(false);
+const toggleLike = () => {
+    setIsLiked(!isLiked); 
+  };
 
-    return (
-        <div>
-            <img 
-            style={{ width: "300px", height: "250px", marginTop: "10px", marginLeft: "10px" }}
-            src={`https://image.tmdb.org/t/p/w500${data?.poster_path}`}
-            onClick={handleClick}
-            />
-            {isModalOpen && <MovieListModal data={data} closeModal={closeModal} />}
-        </div>
-    );
+  const closeModal = () => setIsModalOpen(false);
+
+  return (
+    <div>
+      <img
+        style={{ width: "300px", height: "250px", marginTop: "10px", marginLeft: "10px" }}
+        src={`https://image.tmdb.org/t/p/w500${data?.poster_path}`}
+      />
+      <button id="like" onClick={toggleLike}>
+        {isLiked ? '❤️' : '🤍'}  
+      </button>
+      <button id="quickview" onClick={handleClick}> quick view </button>
+      {isModalOpen && <MovieListModal data={data} closeModal={closeModal} />}
+
+    </div>
+  );
 }
 
 export default MovieContainer;
